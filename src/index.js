107ce98c05e7578fa048293f5c7ce41f7d01e0cc
Natasha Selvidge <Natasha.selvidge@gmail.com>
2021-03-22 19:44:23 -0700

    Set up delete functionality

--- src/index.js
+++ src/index.js
@@ -1,143 +1,61 @@
-let createWorkoutForm = document.querySelector("#create-workout-form");
-
-document.addEventListener("DOMContentLoaded", init);
-const baseUrl = "http://localhost:3000/api/v1/";
 const endPoint = "http://localhost:3000/api/v1/workouts";
+const card = document.querySelector("#workout-container");
 
-function init() {
+document.addEventListener("DOMContentLoaded", () => {
   console.log("Loaded");
   getWorkouts();
-  checkLogin();
-}
 
-const loginDiv = document.getElementById("loginDiv");
-function loadLoginForm() {
-  document.getElementById("loginDiv").innerHTML = `
-  
-          <form id="login-form">
-            <div class="form-group">
-              <h5 class="text-white">Email</h5>
-              <input type="email" class="form-control" id="login-email" aria-describedby="emailHelp">
-              <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-            </div>
-            <div class="form-group">
-              <h5 class="text-white">Password</h5>
-              <input type="password" class="form-control" id="login-password">
-            </div>
-            <button type="submit" class="btn btn-primary">Login</button>
-          </form>
-         
-            
-              
-        
-  `;
-  const loginForm = document.querySelector("#login-form");
-  loginForm.addEventListener("submit", handleLoginForm);
-}
+  let createWorkoutForm = document.querySelector("#create-workout-form");
+  createWorkoutForm.addEventListener("submit", handleCreateForm);
+});
 
-function loadSignupForm() {
-  document.getElementById("signupDiv").innerHTML = `
-    <form id="signUp-form">
-              <label>Email</label>
-                <input type="email" class="form-control" id="signup-email" aria-describedby="emailHelp">
-          
-              <label>Password</label>
-              <input type="password" class="form-control" name="password" id="signUp-password">
-            <button type="submit" class="btn btn-primary">Sign Up</button>
-          </form>
-  `;
-
-  const signUpForm = document.querySelector("#signUp-form");
-  signUpForm.addEventListener("submit", handleSignUpForm);
-}
-
-function getWorkouts(url) {
+//GET
+function getWorkouts() {
   fetch(endPoint)
     .then((response) => response.json())
     .then((workouts) => {
       workouts.data.forEach((workout) => {
         let newWorkout = new Workout(workout, workout.attributes);
-
         document.querySelector(
           "#workout-container"
         ).innerHTML += newWorkout.renderWorkoutCard();
       });
-      // .catch((err) => console.log(err));
     });
-}
-
-function handleLoginForm(e) {
-  handleLoginSignup(e, "login");
-}
-
-function handleSignUpForm(e) {
-  handleLoginSignup(e, "signup");
 }
 
 function handleCreateForm(event) {
   event.preventDefault();
-  const titleInput = event.target.title.value;
-  const descriptionInput = event.target.description.value;
-  const imageInput = event.target.image.value;
-  const categoryId = parseInt(event.target.categories.value);
-  const created_at = event.target.created_at;
-  postFetch(titleInput, descriptionInput, imageInput, categoryId, created_at);
+  const titleInput = document.querySelector("#input-title").value;
+  const descriptionInput = document.querySelector("#input-description").value;
+  const imageInput = document.querySelector("#input-image").value;
+  const categoryId = parseInt(
+    document.querySelector("#input-categories").value
+  );
 
-  createWorkoutForm.addEventListener("submit", handleCreateForm);
+  postFetch(titleInput, descriptionInput, imageInput, categoryId);
 }
-
-function postFetch(title, description, image_url, category_id, created_at) {
-  const dataBody = { title, description, image_url, category_id, created_at };
-  fetch("http://localhost:3000/api/v1/workouts", {
+//POST
+function postFetch(title, description, image_url, category_id) {
+  const dataBody = { title, description, image_url, category_id };
+  fetch(endPoint, {
     //POST request
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(dataBody),
   })
     .then((response) => response.json())
-    .then((workout) => {
-      console.log(workout);
-      const workoutData = workout.data;
-      // render JSON response
-      // render(workoutData);
-      let newWorkout = new Workout(workoutData, workoutData.attributes);
-
-      document.querySelector(
-        "#workout-container"
-      ).innerHTML += newWorkout.renderWorkoutCard();
-    });
+    .then(handleWorkout);
+}
+function handleWorkout(workoutMarkup) {
+  document.querySelector("#workout-container").innerHTML += workoutMarkup;
+  this.location.reload();
 }
 
-function checkLogin() {
-  if (localStorage.jwt_token) {
-    loadSignupForm();
-  } else {
-    loadLoginForm();
-  }
-}
+function handleDelete(event) {
+  let workoutId = parseInt(event.target.dataset.id);
 
-function handleLoginSignup(e, path) {
-  e.preventDefault();
-  const email = e.target[0].value;
-  const password = e.target[1].value;
-  signUpFetch(email, password);
-
-  function signUpFetch(email, password) {
-    const bodyData = {
-      user: {
-        email: email,
-        password: password,
-      },
-    };
-
-    fetch(baseUrl + path, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(bodyData),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        localStorage.setItem("jwt_token", data.jwt);
-      });
-  }
+  fetch(`http://localhost:3000/api/v1/workouts/${workoutId}`, {
+    method: "DELETE",
+  });
+  this.location.reload();
 }
--- src/workout.js
+++ src/workout.js
@@ -1,7 +1,7 @@
 class Workout {
   constructor(workout, workoutAttributes) {
     this.id = workout.id;
-    // debugger;
+
     this.title = workoutAttributes.title;
     this.description = workoutAttributes.description;
     this.image_url = workoutAttributes.image_url;
@@ -13,54 +13,37 @@ class Workout {
 
   renderWorkoutCard() {
     return `
-    
-    <div class="row row-cols-1 row-cols-sm-2 g-3">
-     <div class="col">
-       <div class="card shadow-sm">
-          <img src=${this.image_url} class="card-img-top" height="300" width="250">
-          <div class="card-body">
-            <h2 class="card-title"> ${this.title} </h2>
-            <h5> ${this.category.name} </h5>
-           <div  id="node1" class="d-flex justify-content-between align-items-center" >
-             <div id="node2" class="btn-group" >
 
-               <button type="button" class="btn btn-sm btn-outline-secondary" id="view">
-                 View
-               </button>
-               
-               <button type="button" class="btn btn-sm btn-outline-secondary" id="edit">
-               Edit
-               </button>
-               </div>
-               <small class="text-muted">${this.created_at}</small>
+    <div class="row-cols-1 id="whole-card" >
+      <div class="col" id="whole-card-workout">
+        <div data-id=${this.id}>
+          <div class="card shadow-sm">
+            <img src=${this.image_url} class="card-img-top" height="300" width="250">
+            <div class="card-body" id="card-body">
+              <h2 class="card-title"> ${this.title} </h2>
+              <h5> ${this.category.name} </h5>
+              <p class="card-text">${this.description}</p>
+              <div  id="node1" class="d-flex justify-content-between align-items-center" >
+                <div id="node2" class="btn-group" >
+
+                 <button type="button" class="btn btn-sm btn-outline-secondary" id="edit" data-id=${this.id} >
+                   Edit
+                 </button>
+                 <button onclick="handleDelete(event)" class="btn btn-sm btn-outline-secondary" data-id=${this.id}  >
+                 Delete
+                 </button>
+
+                </div>
+                 <small class="text-muted">${this.created_at}</small>
+              </div>
             </div>
           </div>
         </div>
       </div>
-
-
-    <div class="card border-warning mb-3" style="max-width: 1000%">
-      <div class="card-header">${this.title}</div>
-        <div class="card-body">
-          <h5 class="card-title">${this.category.name}</h5>
-            <p class="card-text">${this.description}</p>
-        </div>
-      </div>
     </div>
-       `;
+                 
+
+         `;
   }
-
-  // renderView() {
-  //   return `
-  // <div class="card border-warning mb-3" style="max-width: 1000%">
-  //   <div class="card-header">${this.title}</div>
-  //   <div class="card-body">
-  //     <h5 class="card-title">${this.category.name}</h5>
-  //     <p class="card-text">${this.description}</p>
-  //   </div>
-  // </div>
-
-  //      `;
-  // }
 }
 Workout.all = [];
