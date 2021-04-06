9c5f6584c00bc638a028e1ab7b1e021fd7efcb47
Natasha Selvidge <Natasha.selvidge@gmail.com>
2021-04-06 16:29:04 -0700

    added search function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,113 @@
 const endPoint = "http://localhost:3000/api/v1/workouts";
-const card = document.querySelector("#workout-container");
+function workoutCard() {
+  return document.getElementById("whole-card");
+}
+const card = () => document.querySelector("#workout-container");
 
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Loaded");
-  getWorkouts();
+const main = () => document.getElementById("main");
+const searchDiv = () => document.getElementById("search");
+const searchForm = () => document.getElementById("search-form");
+const searchInput = () => document.getElementById("search-input");
+const createForm = () => document.getElementById("create-workout-form");
 
-  let createWorkoutForm = document.querySelector("#create-workout-form");
-  createWorkoutForm.addEventListener("submit", handleCreateForm);
-});
+function resetMain() {
+  main().innerHTML = " ";
+}
+
+workoutTemplate = () => {
+  return `
+  <form id="create-workout-form">
+  <div class="mb-3">
+  <label class="form-label" name="title">Title</label>
+  <input
+  name="title"
+  type="title"
+  class="form-control"
+  id="input-title"
+  />
+  
+  <div class="mb-3">
+  <label class="form-label">Description</label>
+  <textarea
+  class="form-control"
+  id="input-description"
+  rows="3"
+  name="description"
+  ></textarea>
+  <label for="exampleInputEmail1">Image Url</label>
+  <input
+  type="img"
+  name="image"
+  class="form-control"
+  id="input-image"
+  />
+  </div>
+  <div class="form-group mb-3">
+  <select
+  class="form-select"
+  name="categories"
+  aria-label="Default select example"
+  id="input-categories"
+  >
+  <option selected>Select type of workout</option>
+  <option value="1" name="categories">ESP</option>
+  <option value="2" name="categories">Enderance</option>
+  <option value="3" name="categories">Strength</option>
+  <option value="4" name="categories">Power</option>
+  </select>
+  </div>
+  
+  <button type="submit" class="btn btn-full">Submit</button>
+  </div>
+  </form> `;
+};
+function showWorkoutForm() {
+  resetMain();
+  main().innerHTML = workoutTemplate();
+}
+
+function renderWorkoutForm() {
+  createForm().addEventListener(
+    "submit",
+    (handleCreateForm = () => {
+      const titleInput = document.querySelector("#input-title").value;
+      const descriptionInput = document.querySelector("#input-description")
+        .value;
+      const imageInput = document.querySelector("#input-image").value;
+      const categoryId = parseInt(
+        document.querySelector("#input-categories").value
+      );
+      postFetch(titleInput, descriptionInput, imageInput, categoryId);
+    })
+  );
+}
+
+function searchFormTemplate() {
+  return `
+<form id="search-form">
+    <label>Search</label>
+      <input type="text" name="search" id="search-input" autofocus="on">
+  </form>`;
+}
+
+function renderForm() {
+  searchDiv().innerHTML = searchFormTemplate();
+}
+
+function search() {
+  searchInput().addEventListener("keyup", function (e) {
+    e.preventDefault();
+    console.log(e.target.value);
+    const term = e.target.value;
+    Workout.all.forEach(function (workout) {
+      if (workout.description.indexOf(term) != -1) {
+        workout.style.display = "block"; //show
+      } else {
+        workout.style.display = "none"; //hide
+      }
+    });
+  });
+}
 
 //GET
 function getWorkouts() {
@@ -31,7 +131,6 @@ function handleCreateForm(event) {
   const categoryId = parseInt(
     document.querySelector("#input-categories").value
   );
-
   postFetch(titleInput, descriptionInput, imageInput, categoryId);
 }
 //POST
@@ -47,15 +146,23 @@ function postFetch(title, description, image_url, category_id) {
     .then(handleWorkout);
 }
 function handleWorkout(workoutMarkup) {
-  document.querySelector("#workout-container").innerHTML += workoutMarkup;
-  this.location.reload();
+  card().innerHTML += workoutMarkup;
 }
 
-function handleDelete(event) {
+handleDelete = (event) => {
   let workoutId = parseInt(event.target.dataset.id);
 
   fetch(`http://localhost:3000/api/v1/workouts/${workoutId}`, {
     method: "DELETE",
   });
-  this.location.reload();
-}
+ 
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Loaded");
+  showWorkoutForm();
+  renderWorkoutForm();
+  getWorkouts();
+  renderForm();
+  search();
+});
--- src/workout.js
+++ src/workout.js
@@ -14,21 +14,20 @@ class Workout {
   renderWorkoutCard() {
     return `
 
-    <div class="row-cols-1 id="whole-card" >
+    <div class="row-cols-1 id="whole-card"  >
       <div class="col" id="whole-card-workout">
+    
         <div data-id=${this.id}>
           <div class="card shadow-sm">
             <img src=${this.image_url} class="card-img-top" height="300" width="250">
             <div class="card-body" id="card-body">
               <h2 class="card-title"> ${this.title} </h2>
               <h5> ${this.category.name} </h5>
-              <p class="card-text">${this.description}</p>
+              <p class="card-text" id="description" data-id=${this.id}>${this.description}</p>
               <div  id="node1" class="d-flex justify-content-between align-items-center" >
                 <div id="node2" class="btn-group" >
 
-                 <button type="button" class="btn btn-sm btn-outline-secondary" id="edit" data-id=${this.id} >
-                   Edit
-                 </button>
+                 
                  <button onclick="handleDelete(event)" class="btn btn-sm btn-outline-secondary" data-id=${this.id}  >
                  Delete
                  </button>
@@ -40,6 +39,7 @@ class Workout {
           </div>
         </div>
       </div>
+   
     </div>
                  
 
